81c39f0ba2b260429a30f8a9b5aacfc01657364a
alexdneprov <alexdneprov1@gmail.com>
2024-07-07 15:59:46 -0700

    changed app.js

--- src/app.js
+++ src/app.js
@@ -2,10 +2,11 @@ if (process.env.USER) require("dotenv").config();
 const errorHandler = require("./errors/errorHandler");
 const cors = require('cors'); 
 
+const app = express();
 app.use(cors());
 
 const express = require("express");
-const app = express();
+
 const moviesRouter = require("./movies/movies.router");
 const theatersRouter = require("./theaters/theaters.router");
 const reviewsRouter = require("./reviews/reviews.router");
